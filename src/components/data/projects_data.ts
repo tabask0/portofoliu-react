47e984f32d3e704423605051855207c3fe4c804a
tabask0 <bucur37@gmail.com>
2021-04-29 01:02:20 +0300

    updated

--- src/components/data/projects_data.ts
+++ src/components/data/projects_data.ts
@@ -1,85 +1,86 @@
-import angular from '../../assets/projects_images/angular.jpg'
-import kanban from '../../assets/projects_images/kanban.jpg'
-import payments from '../../assets/projects_images/payments.jpg'
-import portfolio from '../../assets/projects_images/portfolio.jpg'
-import stan from '../../assets/projects_images/stan.jpg'
-import tictac from '../../assets/projects_images/tictac.jpg'
-import todo from '../../assets/projects_images/todo.jpg'
-import weather from '../../assets/projects_images/weather.jpg'
-import wordpress from '../../assets/projects_images/wordpress.jpg'
-import crypto from '../../assets/projects_images/crypto.jpg'
+import angular from "../../assets/projects_images/angular.jpg";
+import kanban from "../../assets/projects_images/kanban.jpg";
+import payments from "../../assets/projects_images/payments.jpg";
+import portfolio from "../../assets/projects_images/portfolio.jpg";
+import stan from "../../assets/projects_images/stan.jpg";
+import tictac from "../../assets/projects_images/tictac.jpg";
+import todo from "../../assets/projects_images/todo.jpg";
+import weather from "../../assets/projects_images/weather.jpg";
+import wordpress from "../../assets/projects_images/wordpress.jpg";
+import crypto from "../../assets/projects_images/crypto.jpg";
 
 const data_projects = [
-    {
-        name: 'Angular Character System',
-        image: angular,
-        deployed_url: 'https://stackblitz.com/edit/angular-ivy-y4rgap',
-        github_url: 'https://github.com/tabask0/aplicati',
-        category: ['framework']
-    },
-    {
-        name: 'Crypto API React',
-        image: crypto,
-        deployed_url: 'https://cryptoapi-55c7d.web.app/',
-        github_url: 'https://github.com/tabask0/react-crypto-api',
-        category: ['framework']
-    },
-    {
-        name: 'Kanban Board',
-        image: kanban,
-        deployed_url: 'https://tasks-manager-49c08.web.app/',
-        github_url: 'https://github.com/tabask0/task-management',
-        category: ['framework']
-    },
-    {
-        name: 'Angular Payment App',
-        image: payments,
-        deployed_url: 'https://payment-preview.firebaseapp.com/',
-        github_url: 'https://github.com/tabask0/payments',
-        category: ['framework']
-    },
-    {
-        name: 'Stan Boutique Shop',
-        image: stan,
-        deployed_url: 'https://tabask0.github.io/StanBoutique/',
-        github_url: 'https://github.com/tabask0/stanBoutique/tree/main/StanBoutique',
-        category: ['html-css-js']
-    },
-    {
-        name: 'Angular Tic-tac-toe',
-        image: tictac,
-        deployed_url: 'https://emency-31de2.firebaseapp.com/',
-        github_url: 'https://github.com/tabask0/x-si-o',
-        category: ['framework']
-    },
-    {
-        name: 'Portfolio',
-        image: portfolio,
-        deployed_url: 'https://tabask0.github.io/codiver/',
-        github_url: 'https://github.com/tabask0/codiver.ro',
-        category: ['html-css-js']
-    },
-    {
-        name: 'Todo List',
-        image: todo,
-        deployed_url: 'https://tabask0.github.io/TodoApp/',
-        github_url: 'https://github.com/tabask0/todo',
-        category: ['html-css-js']
-    },
-    {
-        name: 'Weather App',
-        image: weather,
-        deployed_url: 'https://tabask0.github.io/codiver/WeatherApp/',
-        github_url: 'https://github.com/tabask0/weather-app',
-        category: ['html-css-js']
-    },
-    {
-        name: 'Wordpress Website',
-        image: wordpress,
-        deployed_url: 'https://scoalamionescu.ro/',
-        github_url: '#',
-        category: ['wordpress']
-    }
-]
+  {
+    name: "Angular Character System",
+    image: angular,
+    deployed_url: "https://stackblitz.com/edit/angular-ivy-y4rgap",
+    github_url: "https://github.com/tabask0/aplicati",
+    category: ["framework"],
+  },
+  {
+    name: "Crypto API React",
+    image: crypto,
+    deployed_url: "https://cryptoapi-55c7d.web.app/",
+    github_url: "https://github.com/tabask0/react-crypto-api",
+    category: ["framework"],
+  },
+  {
+    name: "Kanban Board",
+    image: kanban,
+    deployed_url: "https://tasks-manager-49c08.web.app/",
+    github_url: "https://github.com/tabask0/task-management",
+    category: ["framework"],
+  },
+  {
+    name: "Angular Payment App",
+    image: payments,
+    deployed_url: "https://payment-preview.firebaseapp.com/",
+    github_url: "https://github.com/tabask0/payments",
+    category: ["framework"],
+  },
+  {
+    name: "Stan Boutique Shop",
+    image: stan,
+    deployed_url: "https://tabask0.github.io/StanBoutique/",
+    github_url:
+      "https://github.com/tabask0/stanBoutique/tree/main/StanBoutique",
+    category: ["html-css-js"],
+  },
+  {
+    name: "Angular Tic-tac-toe",
+    image: tictac,
+    deployed_url: "https://emency-31de2.firebaseapp.com/",
+    github_url: "https://github.com/tabask0/x-si-o",
+    category: ["framework"],
+  },
+  {
+    name: "Portfolio",
+    image: portfolio,
+    deployed_url: "https://tabask0.github.io/codiver/",
+    github_url: "https://github.com/tabask0/codiver.ro",
+    category: ["html-css-js"],
+  },
+  {
+    name: "Todo List",
+    image: todo,
+    deployed_url: "https://tabask0.github.io/TodoApp/",
+    github_url: "https://github.com/tabask0/todo",
+    category: ["html-css-js"],
+  },
+  {
+    name: "Weather App",
+    image: weather,
+    deployed_url: "https://tabask0.github.io/codiver/WeatherApp/",
+    github_url: "https://github.com/tabask0/weather-app",
+    category: ["html-css-js"],
+  },
+  {
+    name: "Wordpress Website",
+    image: wordpress,
+    deployed_url: "https://scoalamionescu.ro/",
+    github_url: "#",
+    category: ["wordpress"],
+  },
+];
 
 export default data_projects;
